b85c5fcded5107ed906569a479c71f1216137d36
gilas <inigogil@bilbokoeskolapioak.org>
2024-04-06 14:42:30 +0200

    Directory changes

--- nuclei_analysis.py
+++ nuclei_analysis.py
@@ -1,6 +1,7 @@
 import pymongo
 import subprocess
 from dotenv import load_dotenv
+import os
 
 
 load_dotenv()
@@ -18,7 +19,12 @@ with open("targets.txt", "w") as file:
     for company in companies:
         file.write(company["web"] + "\n")
 
-result = subprocess.run(["docker", "run", "-v", "$(pwd):/nuclei", "projectdiscovery/nuclei",
-            "-l", "/nuclei/targets.txt"], capture_output=True, text=True)
+exit()
+subprocess.run(["docker", "compose", "start", "nuclei"])
+
+result = subprocess.run(["docker", "compose", "exec", "-T", "nuclei", "nuclei",
+            "-l", "/nuclei/targets.txt", "-o", "/nuclei/results.json"], capture_output=True, text=True)
 
 print(result.stdout)
+
+subprocess.run(["docker", "compose", "stop", "nuclei"])
