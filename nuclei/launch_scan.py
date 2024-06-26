d75c03d163428ef7ae1e92a82af06c3aa59d1e50
gilas <inigogil@bilbokoeskolapioak.org>
2024-06-26 15:50:57 +0200

    [MISC] Scan can now be run with local Nuclei instalation via parameter

--- nuclei/launch_scan.py
+++ nuclei/launch_scan.py
@@ -7,8 +7,11 @@ import os
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-y", action="store_true", help="Skip confirmation")
-parser.add_argument("-n", type=int, help="Number of companies to scan")
+parser.add_argument("-y", "--skip-confirmation", action="store_true", help="Skip confirmation")
+parser.add_argument("-n", "--number-companies", type=int, help="Number of companies to scan")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-d", "--docker", action="store_true", help="Run analysis inside docker")
+group.add_argument("-l", "--local", action="store_true", help="Run analysis with the local tool")
 args = parser.parse_args()
 
 load_dotenv()
@@ -31,10 +34,10 @@ except Exception as e:
     print(e)
     exit()
 
-if args.n:
+if args.number_companies:
     pipeline = [
         {"$group": {"_id": "$web", "doc": {"$first": "$$ROOT"}}},
-        {"$sample": {"size": args.n}},
+        {"$sample": {"size": args.number_companies}},
         {"$replaceRoot": {"newRoot": "$doc"}}
     ]
     companies_urls = [doc['web'] for doc in collection.aggregate(pipeline)]
@@ -54,29 +57,31 @@ with open(targets_file_path, "w") as file:
 print(f"Added {num_companies} companies to targets.txt")
 
 # Ask if the user wants to launch the scan
-if not args.y:
+if not args.skip_confirmation:
     print(f"{num_companies} targets are going to be scanned. Do you want continue? (y/n): ")
     answer = input().lower()
     if answer != "y":
-        print("Scana canceled")
+        print("Scan canceled")
         exit()
 
 current_date = datetime.now().strftime("%Y-%m-%d")
 file_name = f"scan-result-{current_date}.json"
 shared_volume_path = os.path.join(current_dir, "shared-volume")
-command = f"docker run -v {shared_volume_path}:/go/src/app:rw \
---rm --net=container:vpn projectdiscovery/nuclei:latest \
--l /go/src/app/targets.txt -j -config /go/src/app/rise-config.yml > {shared_volume_path}/results/{file_name}"
+if args.docker:
+    # Run analysis inside docker
+    command = f"docker run -v {shared_volume_path}:/go/src/app:rw \
+    --rm --net=container:vpn projectdiscovery/nuclei:latest \
+    -l /go/src/app/targets.txt -j -config /go/src/app/rise-config.yml > {shared_volume_path}/results/{file_name}"
+else:
+    # Default option
+    # Run analysis with the local tool
+    command = f"nuclei -l {targets_file_path} -j -config {shared_volume_path}/rise-config.yml > {shared_volume_path}/results/{file_name}"
 
 try:
+    print("Launching scan . . .")
     subprocess.run(command, shell=True)
 except Exception as e:
     print("Error launching scan")
     print(e)
     exit()
-finally:
-    print("Executing finally clause . . .")
-    # Print system resources
-    subprocess.run("free -h", shell=True)
-
 
