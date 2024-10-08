d09d941fadcceea905ed26865467247e624dc375
gilas <inigogil@bilbokoeskolapioak.org>
2024-10-08 20:08:26 +0200

    [MISC] DB_HOST env variable must be different between inside-docker use and outside-docker use

--- nuclei/db_manager.py
+++ nuclei/db_manager.py
@@ -7,7 +7,7 @@ load_dotenv()
 DATABASE_URL = (
            f'mongodb://{os.getenv("MONGO_USER")}:'
            f'{os.getenv("MONGO_PASS")}@'
-           f'{os.getenv("DB_HOST")}:'
+           f'{os.getenv("DB_HOST_OUTS_DOCK")}:'
            f'{os.getenv("DB_PORT")}'
            )
 DATABASE_NAME = os.getenv("DB_NAME")
