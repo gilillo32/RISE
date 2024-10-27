f44b6df156608c8bc31412e7a992c11efaf28c2f
gilas <inigogil@bilbokoeskolapioak.org>
2024-10-27 22:33:05 +0100

    [MISC] Changed processing logic

--- nuclei/process_scan_results.py
+++ nuclei/process_scan_results.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import decimal
 from db_manager import DbManager
 from tqdm import tqdm
@@ -18,27 +19,40 @@ def convert_decimal_to_float(data):
         return float(data)
     return data
 
+def process_file(filename):
+    try:
+        db_manager.archive_last_scan()
+        # Load the scan result
+        print("Opening scan result file")
+        with open(filename, "r") as file:
+            print("Starting loop")
+            for item in tqdm(ijson.items(file, "item"), desc="Processing items", unit="item", ascii=" ▖▘▝▗▚▞█"):
+                converted_item = convert_decimal_to_float(item)
+                db_manager.save_last_scan_item(converted_item)
+        # Move the processed file
+        processed_dir = os.path.join(os.path.dirname(filename), "processed")
+        os.makedirs(processed_dir, exist_ok=True)
+        shutil.move(filename, os.path.join(processed_dir, os.path.basename(filename)))
+    except Exception as e:
+        print("Error loading scan result")
+        print(type(e))
+        print(str(e))
+        print(e)
+
 # Get the filename of the scan result from arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("filename", help="Scan result filename")
+parser.add_argument("-f", "--filename", help="Scan result filename")
 args = parser.parse_args()
-filename = args.filename
 
-try:
-    db_manager.archive_last_scan()
-    # Load the scan result
-    print("Opening scan result file")
-    with open(filename, "r") as file:
-        print("Starting loop")
-        for item in tqdm(ijson.items(file, "item"), desc="Processing items", unit="item", ascii=" ▖▘▝▗▚▞█"):
-            converted_item = convert_decimal_to_float(item)
-            db_manager.save_last_scan_item(converted_item)
-except Exception as e:
-    print("Error loading scan result")
-    print(type(e))
-    print(str(e))
-
-    print(e)
+if args.filename:
+    process_file(args.filename)
+else:
+    # Get the path of the script:
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    results_dir = os.path.join(script_path, "shared-volume", "results")
+    for filename in os.listdir(results_dir):
+        if filename.endswith(".json"):
+            process_file(os.path.join(results_dir, filename))
 
 print("Scan result processed")
 
